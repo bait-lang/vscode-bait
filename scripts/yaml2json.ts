9447df5b12ffb5e412692d5023df9dd6d3284e8c
Lukas Neubert <serkonda7@pm.me>
2022-04-11 19:12:32 +0200

    convert grammar to yaml

--- scripts/yaml2json.ts
+++ scripts/yaml2json.ts
@@ -3,18 +3,19 @@ import * as fs from 'fs'
 import * as path from 'path'
 
 const yml_paths = [
-	'./language-configuration.yml'
+	'./language-configuration.yml',
+	'./syntaxes/bait.tmGrammar.yml',
 ]
 
 const get_out_path = (inpath: string): string => {
-	const out_path = path.resolve(__dirname, '..', 'out', inpath)
+	const out_path = path.resolve(__dirname, '..', 'out', path.basename(inpath))
 	return out_path.replace('.yml', '.json')
 }
 
 const convert_file = (yml_path: string) => {
 	const yml_txt = fs.readFileSync(yml_path, 'utf8')
 	const obj = yaml.load(yml_txt)
-	const json_txt = JSON.stringify(obj) + '\n'
+	const json_txt = JSON.stringify(obj, null, 2) + '\n'
 	fs.writeFileSync(get_out_path(yml_path), json_txt)
 }
 

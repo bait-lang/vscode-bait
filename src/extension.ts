c92e75d14dd483d155fed88f0ddeb5cc01e5ef98
Lukas Neubert <serkonda7@pm.me>
2023-05-30 23:03:55 +0200

    replace TypeScript with Bait (#12)

--- src/extension.ts
+++
@@ -1,9 +0,0 @@
-// This file is part of vscode-bait.
-// Copyright (c) 2022-present Lukas Neubert.
-// This Source Code Form is subject to the terms of the Mozilla Public License 2.0.
-
-import * as vscode from 'vscode';
-
-export function activate(context: vscode.ExtensionContext) {
-	console.log('vscode-bait activated.')
-}
